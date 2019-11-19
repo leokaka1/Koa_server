cbe509312fb88af4ebff3a598252e5d820fe40eb
leokaka1 <leocaoxiaozhu@163.com>
2019-11-19 10:13:25 +0800

    调整了引入结构，加入了validator验证输入项

--- app.js
+++ app.js
@@ -3,29 +3,29 @@ const Router = require('koa-router');
 const Mongoose = require('mongoose')
 // config
 const Config = require('./config/keys')
-// 引入users
+// 引入user用户模块
 const users = require('./routes/api/users')
 // 引入body-parser接收参数
 const bodyParser = require('koa-bodyparser')
 // 引入验证工具passport
 const passport = require('koa-passport')
-
-
 // 实例化koa
 const app = new Koa();
 const router = new Router();
 //引入db地址
 const db = Config.mongoURI
-// 配置路由地址
-router.use('/api/users',users)
 // 使用bodyParser
 app.use(bodyParser())
 // 初始化passport
 app.use(passport.initialize())
-// app.use(passport.session())
+app.use(passport.session())
 require('./config/passport')(passport)
 // 配置路由
 app.use(router.routes()).use(router.allowedMethods());
+
+// ☆☆☆☆配置路由地址☆☆☆☆
+// 用户请求接口
+router.use('/api/users',users)
 
 // 链接数据库
 Mongoose.connect(
@@ -34,8 +34,10 @@ Mongoose.connect(
 { useNewUrlParser: true,
   useUnifiedTopology: true
 }).then(()=>{
+    // 数据库连接成功
     console.log("☆☆☆☆☆☆mongodb is connected!!!!☆☆☆☆☆☆")
 }).catch(error=>{
+    // 连接失败的Log
     console.log(error)
 })
 
