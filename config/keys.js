7fbcaaed4b3211342a536be05f80fd11d615b838
leokaka1 <leocaoxiaozhu@163.com>
2019-11-20 09:29:06 +0800

    修改了mongodb的链接

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,7 @@
 module.exports ={
-    mongoURI:"mongodb+srv://leokaka1:88116142@cluster0-dvjgh.mongodb.net/test?retryWrites=true&w=majority",
+    // 远程mongo,需要绑定ip
+    // mongoURI:"mongodb+srv://leokaka1:88116142@cluster0-dvjgh.mongodb.net/test?retryWrites=true&w=majority",
+    // 自动mongo
+    mongoURI:"mongodb://leokaka1.wicp.vip:22196",
     tokenKey:"secret"
 }
